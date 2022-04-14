856f47681e71c1f765ef1696641a29302d0b7575
Inesa M <inesa.milker@gmail.com>
2022-04-14 11:47:47 +0300

    nothing changed

--- app/Models/Trips.php
+++ app/Models/Trips.php
@@ -7,8 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Trips extends Model
 {
-    use HasFactory;
-    protected $fillable = [
-        'title', 'start_date', 'end_date', 'rating', 'user_id'
-    ];
+  use HasFactory;
+  protected $fillable = [
+    "title",
+    "start_date",
+    "end_date",
+    "rating",
+    "user_id",
+  ];
 }
