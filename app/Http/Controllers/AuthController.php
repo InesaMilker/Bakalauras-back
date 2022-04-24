45fbb4e32695016831780a0b79b107cfdd33839f
Inesa M <inesa.milker@gmail.com>
2022-04-24 20:02:19 +0300

    trip date update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,96 +7,104 @@ use App\Models\Trips;
 use App\Models\Diary;
 use App\Models\Outfit;
 use App\Models\Checklist;
+use App\Models\Clothes;
+use App\Models\Images;
+use App\Models\Links;
+use App\Models\TripLinks;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+  public function __construct()
+  {
+    $this->middleware("auth:api", ["except" => ["login", "register"]]);
+  }
+
+  public function login()
+  {
+    $credentials = request(["email", "password"]);
+
+    if (!($token = auth()->attempt($credentials))) {
+      return response()->json(["error" => "Invalid email or password"], 401);
     }
 
-    public function login()
-    {
-        $credentials = request(['email', 'password']);
+    return $this->respondWithToken($token);
+  }
 
-        if (!$token = auth()->attempt($credentials))
-        {
-            return response()
-            ->json(['error' => 'Invalid email or password'], 401);
-        }
+  protected function respondWithToken($token)
+  {
+    return response()->json([
+      "access_token" => $token,
+      "token_type" => "bearer",
+      "expires_in" =>
+        auth("api")
+          ->factory()
+          ->getTTL() * 60,
+    ]);
+  }
 
-        return $this->respondWithToken($token);
+  public function logout()
+  {
+    auth()->logout();
+    return response()->json(["message" => "Successfully logged out"]);
+  }
+
+  public function refresh()
+  {
+    return $this->respondWithToken(auth()->refresh());
+  }
+
+  public function me()
+  {
+    return response()->json(auth()->user());
+  }
+
+  public function register(Request $request)
+  {
+    $validator = Validator::make($request->all(), [
+      "name" => "required|string|between:2,100",
+      "email" => "required|string|email|max:100|unique:users",
+      "password" => "required|string|confirmed|min:6",
+    ]);
+
+    if ($validator->fails()) {
+      return response()->json($validator->errors()->toJson(), 400);
     }
 
-    protected function respondWithToken($token)
-    {
-        return response()
-        ->json(['access_token' => $token, 'token_type' => 'bearer', 
-        'expires_in' => auth('api')->factory()
-            ->getTTL() * 60, ]);
+    $user = User::create(
+      array_merge($validator->validated(), [
+        "password" => bcrypt($request->password),
+      ])
+    );
+
+    return response()->json(
+      ["message" => "User successfully registered", "user" => $user],
+      201
+    );
+  }
+
+  public function destroy()
+  {
+    $user_id = auth()->user()->id;
+    $user = User::find($user_id);
+
+    Coordinates::where("user_id", $user_id)->delete();
+    Day::where("user_id", $user_id)->delete();
+    Diary::where("user_id", $user_id)->delete();
+    Trips::where("user_id", $user_id)->delete();
+    Checklist::where("user_id", $user_id)->delete();
+    Outfit::where("user_id", $user_id)->delete();
+    Clothes::where("user_id", $user_id)->delete();
+    Images::where("user_id", $user_id)->delete();
+    Links::where("user_id", $user_id)->delete();
+    TripLinks::where("user_id", $user_id)->delete();
+
+    $user->delete();
+
+    if ($user->delete()) {
+      return response()->json(["success"], 200);
     }
-
-    public function logout()
-    {
-
-        auth()
-            ->logout();
-        return response()
-            ->json(['message' => 'Successfully logged out']);
-    }
-
-    public function refresh()
-    {
-        return $this->respondWithToken(auth()->refresh());
-    }
-
-    public function me()
-    {
-        return response()
-            ->json(auth()
-            ->user());
-    }
-
-    public function register(Request $request)
-    {
-        $validator = Validator::make($request->all() , 
-        ['name' => 'required|string|between:2,100', 
-        'email' => 'required|string|email|max:100|unique:users', 
-        'password' => 'required|string|confirmed|min:6', ]);
-
-        if ($validator->fails())
-        {
-            return response()
-                ->json($validator->errors()
-                ->toJson() , 400);
-        }
-
-        $user = User::create(array_merge($validator->validated() ,
-         ['password' => bcrypt($request->password) ]));
-
-        return response()
-        ->json(['message' => 'User successfully registered', 'user' => $user], 201);
-    }
-
-    public function destroy()
-    {
-        $user_id = auth()->user()->id;
-        $user = User::find($user_id);
-
-        Coordinates::where('user_id', $user_id)->delete();
-        Day::where('user_id', $user_id)->delete();
-        Diary::where('user_id', $user_id)->delete();
-        Trips::where('user_id', $user_id)->delete();
-        Checklist::where('user_id', $user_id)->delete();
-        Outfit::where('user_id', $user_id)->delete();
-
-        $user->delete();
-        
-        if($user->delete()){
-            return response()->json(['success'], 200);
-        }
-    }
+  }
 }
--- app/Http/Controllers/TripsApiController.php
+++ app/Http/Controllers/TripsApiController.php
@@ -7,6 +7,7 @@ use App\Models\Day;
 use App\Models\Diary;
 use App\Models\Trips;
 use Illuminate\Http\Request;
+use DateTime;
 
 class TripsApiController extends Controller
 {
@@ -63,7 +64,58 @@ class TripsApiController extends Controller
 
         if (Trips::where("id", $id)->exists()) {
           $trips = Trips::find($id);
+          if (!(is_null($request->start_date) && is_null($request->end_date))) {
+            $start_old = $trips->start_date;
+            $start_new = $request->start_date;
+            $datetime1_start = new DateTime($start_old);
+            $datetime2_start = new DateTime($start_new);
+            $interval_start = $datetime1_start->diff($datetime2_start);
 
+            if ($interval_start->invert == "1") {
+              $old_start_days = Day::where("trip_id", $id)->get();
+              foreach ($old_start_days as $old_day) {
+                $old_day->day_number =
+                  $old_day->day_number + $interval_start->days;
+                $old_day->save();
+              }
+            }
+
+            if (
+              $interval_start->invert == "0" &&
+              $interval_start->days != "0"
+            ) {
+              $old_start_days = Day::where("trip_id", $id)->get();
+              foreach ($old_start_days as $old_day) {
+                if ($old_day->day_number - $interval_start->days <= 0) {
+                  $old_day->delete();
+                  return;
+                }
+                $old_day->day_number =
+                  $old_day->day_number - $interval_start->days;
+                $old_day->save();
+              }
+            }
+
+            $end_old = $trips->end_date;
+            $end_new = $request->end_date;
+            $datetime1_end = new DateTime($end_old);
+            $datetime2_end = new DateTime($end_new);
+            $interval_end = $datetime1_end->diff($datetime2_end);
+
+            $interval_of_new_dates = $datetime2_start->diff($datetime2_end);
+
+            if ($interval_end->invert == "1") {
+              $old_end_days = Day::where("trip_id", $id)->get();
+              foreach ($old_end_days as $old_day) {
+                if (
+                  $old_day->day_number - ($interval_of_new_dates->days + 1) >
+                  0
+                ) {
+                  $old_day->delete();
+                }
+              }
+            }
+          }
           if ($user_id == $trips->user_id) {
             $trips->title = is_null($request->title)
               ? $trips->title
